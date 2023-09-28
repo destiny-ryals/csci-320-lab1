8c5394874f3fce2c38ee08ab9a6ee3a058d6c34a
destiny-ryals <destinycryals@gmail.com>
2023-09-28 14:34:13 +0000

    updates to lab1.c

--- lab1.c
+++ lab1.c
@@ -1,5 +1,12 @@
-/*
- * Implement your solution in thi file
- *
- */
+#include <stdio.h> /*defines three variable types, several macros, and various functions for performing input and output.*/
+#include <stdlib.h> /*defines four variable types, several macros, and various functions for performing general functions.*/
+#include <string.h>/*defines one variable type, one macro, and various functions for manipulating arrays of characters.*/
+
+char* readString(char* fileName){/*declares a function that takes a C- string representing the filename as input and returns a C-string as the output*/
+
+}
+
+char* mysteryExplode(const char* str){
+    
+}
 
