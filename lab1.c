eddc08068d6b2419d1d413c95789a4e62b619cb8
destiny-ryals <destinycryals@gmail.com>
2023-09-30 00:31:09 +0000

    complete explodedString

--- lab1.c
+++ lab1.c
@@ -7,7 +7,7 @@ char* readString(char* fileName){/*declares a function that takes a C- string re
 /*open the file 'fileName' in read mode ("r")
 if file cannot be opened, print an error message and return NULL*/
 
-FILE* file = fopen(file, "r");
+FILE* file = fopen(fileName, "r");
 if(file == NULL){
     perror("Error opening file");
     return NULL;
@@ -32,11 +32,27 @@ return string;
 
 char* mysteryExplode(const char* str){
     /*find the length of the string*/
+    int str_length = strlen(str);
     /*calculate the length of the exploded string N*(N+1)/2 +1 (+1 is for the null mark at the end of the string)*/
+    int exploded_length = str_length*(str_length + 1)/2 +1;
     /*dynamically allocate memory that is the size of our exploded string*/
+    char* explodedString = (char*)malloc(exploded_length);
+    if(explodedString == NULL){
+        return NULL;
+    }
     /*initialize a pointer to the beginning of the exploded string*/
+    char* current_position = explodedString;
     /* create a for loop which iterates through the characters of the string*/
+    for(int i=0; i<str_length; i++){
+        /*create a for loop that iterates from j to 1 and copy the character at current_position and then advance the pointer*/
+        for(int j = 0; j<= i; j++){
+            *current_position = str[j];
+            current_position++;
+        }
+    }
     /*null terminate the string*/
+    *current_position = '\0';
     /*return the exploded string*/
+    return explodedString;
 }
 
